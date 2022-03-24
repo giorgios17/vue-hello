051e43406459c80708cefeb1c9cfed52ac123c2c
giorgios17 <giosur@live.it>
2022-03-24 18:11:44 +0100

    test

--- js/script.js
+++ js/script.js
@@ -6,7 +6,13 @@ const app = new Vue(
         el: '#vue',
         data: {
             message: 'Sto imparando ad usare Vue',
-            eldenRingImg: 'img/elden_1.0.jpg'
+            eldenRingImg: 'img/elden_1.0.jpg',
+            contatore: '10'
         },
+        methods: {
+            cambiaImg() {
+                this.eldenRingImg = 'https://p325k7wa.twic.pics/high/elden-ring/elden-ring/00-page-setup/elden-ring-new-header-mobile.jpg?twic=v1/cover=760x844/step=10/quality=80'
+            }
+        }
     }
 );
